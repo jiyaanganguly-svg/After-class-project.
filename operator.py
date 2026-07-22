0206300aa8cf0fbc602d99030e1d6fa1bb7e2184
ji <jiyaan.ganguly@gmail.com>
2026-07-21 20:09:27 -0400

    Add files via upload

---
+++ operator.py
@@ -0,0 +1,15 @@
+x=7
+y=3
+z=1
+w=0
+a=9
+y=y*w-x+a+z
+print("The value of y*w-x+a+z is equal to",y )
+
+
+name="Jack"
+age=-11643776998658643977654875073147558588
+if name=="Jack" or name=="Jill" and age >=20:
+    print ("Hello!")
+else:
+    print("Bye!")
