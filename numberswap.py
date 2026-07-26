3983510de29ebead158be46a2dc34ddf489148f5
ji <jiyaan.ganguly@gmail.com>
2026-07-25 20:13:46 -0400

    Add files via upload

---
+++ numberswap.py
@@ -0,0 +1,14 @@
+#Initial Values
+a=7
+b=5
+c=10
+d=9
+x=a*(b+c)**d
+print("The value of x in the initial wrong problem is", x)
+#Swapping Value
+a=5
+b=10
+c=7
+d=9
+x=a*(b+c)**d
+print("The value of x after doing the correct values is", x)
