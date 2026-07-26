fecc574fa0a113c6e58f14d61736d00063b17d79
ji <jiyaan.ganguly@gmail.com>
2026-07-26 15:18:12 -0400

    Add files via upload

---
+++ meanvalue.py
@@ -0,0 +1,9 @@
+wrongmean=38
+wrongnum=36
+correctnum=56
+totalvaribles=40
+total=wrongmean*totalvaribles
+num2=total-((wrongnum)-(correctnum))
+print("Correct total=sum+((correctnum)-(wrongnum))=", num2)
+mean=num2/totalvaribles
+print("Correct mean=num2/totalvaribles", mean)
